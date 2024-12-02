99b7cb233ac62c98c37a9d0d3aaad25bbce1484c
Nil <nenakhov.neil@mail.ru>
2024-12-02 21:21:30 +0300

    locale

--- Show.c
+++ Show.c
@@ -4,4 +4,5 @@ int main(int argc, char* argv[]) {
                 printf("Too few arguments. Specify path to file you wan to display\n");
                 return 1;
         }
+        setlocale(LC_ALL, "");
 }
