9f855a2e3a706756dcf63f5280bbdce75abe17ed
Nil <nenakhov.neil@mail.ru>
2024-12-02 21:20:53 +0300

    main funciton

---
+++ Show.c
@@ -0,0 +1,7 @@
+
+int main(int argc, char* argv[]) {
+        if (argc < 2) {
+                printf("Too few arguments. Specify path to file you wan to display\n");
+                return 1;
+        }
+}
