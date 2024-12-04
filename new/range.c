d67c05a16a0c2b11de0776a800fcd31c7d573ffa
Nil <nenakhov.neil@mail.com>
2024-12-04 14:06:52 +0300

    S added

--- new/range.c
+++ new/range.c
@@ -5,6 +5,7 @@ int main(int argc, char const *argv[])
 {
 	int N = (argc == 2) ? atoi(argv[1]) : atoi(argv[2]);
 	int M = (argc == 2) ? 0 : atoi(argv[1]);
+	int S = (argc == 4) ? atoi(argv[3]) : 1;
 
 	return 0;
 }
