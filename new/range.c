6011727b988a0951afe66ce229d2914994c2dbc1
Nil <nenakhov.neil@mail.com>
2024-12-04 14:06:32 +0300

    range

---
+++ new/range.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int N = (argc == 2) ? atoi(argv[1]) : atoi(argv[2]);
+	int M = (argc == 2) ? 0 : atoi(argv[1]);
+
+	return 0;
+}
