02159ced84f9ea4cca27c1afe65ab93731c90159
Nil <nenakhov.neil@mail.com>
2024-12-04 13:57:27 +0300

    get data

--- new/randomize.sh
+++ new/randomize.sh
@@ -6,4 +6,26 @@ raw=`hexdump -e '16/1 "%02x\n" "\n"'`
 col=0
 row=0
 
+while IFS= read -r line
+do
+    # echo $line
+    case "$line" in
+        "0a")
+            row=$((++row))
+            col=0
+            ;;
+        "20")
+            col=$((++col))
+            ;;
+        "  ")
+            ;;
+        *)
+            data+="$row $col $line"$'\n'
+            col=$((++col))
+            ;;
+    esac
+done < <(printf  "%s\n" "$raw")
+
+
+
 tput cup $LINES 0
