8bfbd694e9387e2317224919af4a6756ef371618
Nil <nenakhov.neil@mail.ru>
2024-12-08 18:06:19 +0300

    rhasher.c

---
+++ new/rhasher.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include "rhash.h"
+
+#ifdef USE_READLINE
+#include <readline/readline.h>
+#endif
+
+int main(int argc, char const *argv[])
+{
+
+}
