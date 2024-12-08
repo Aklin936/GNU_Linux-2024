be6811a878a42109b5b013c59a643cdd26b2444e
Nil <nenakhov.neil@mail.ru>
2024-12-08 17:56:05 +0300

    now work again

--- 06_Tracing/remove.c
+++ 06_Tracing/remove.c
@@ -6,3 +6,10 @@ typedef int (*remove_type)(const char *pathname);
 
 int remove(const char *pathname) {
     remove_type real_remove = dlsym(RTLD_NEXT, "remove");
+
+    if (strstr(pathname, "PROTECT") != NULL) {
+        return 0;
+    }
+
+    return real_remove(pathname);
+}
