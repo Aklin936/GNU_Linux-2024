5386dc19331784e9b19eec53336208365e73529f
Nil <nenakhov.neil@mail.ru>
2024-12-08 18:08:43 +0300

    flags

--- new/rhasher.c
+++ new/rhasher.c
@@ -11,5 +11,31 @@
 
 int main(int argc, char const *argv[])
 {
+	unsigned char *digest = (unsigned char *) malloc(64 * sizeof(unsigned char));
+	char output[130];
 
+	rhash_library_init();
+
+	const char *delim = " ";
+
+#ifndef USE_READLINE
+	size_t linesize = 100;
+	char *line = (char *) malloc(linesize * sizeof(char));
+
+		line[strcspn(line, "\n")] = 0;
+		char *arg1 = strtok(line, delim);
+		char *arg2 = strtok(NULL, delim);
+
+		int FLAGS = 0;
+		if (isupper(arg1[0])) {
+			FLAGS |= RHPR_HEX;
+		} else {
+			FLAGS |= RHPR_BASE64;
+		}
+
+
+		printf("%s (%s) = %s\n", rhash_get_name(code), arg2, output);
+	}
+
+	return 0;
 }
